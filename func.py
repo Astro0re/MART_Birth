89a98cc70a35d63298cf9234c62ac5a40effdbfe
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2026-05-09 00:18:15 +0100

    Code Update

--- func.py
+++ func.py
@@ -20,7 +20,7 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise ValueError("SUPABASE_URL and SUPABASE_KEY environment variables must be set")
 
-supabase : Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 client.send
 
@@ -36,3 +36,14 @@ if st.button("Submit", key='SUB'):
             }
     
         add_values('birth_track', data)
+
+# Supabase input command
+supabase.table("").insert({
+            f_name : st.session_state["f_name_d"],
+            l_name : st.session_state["l_name_d"], 
+            cycle_id : st.session_state["cycle_id_d"], 
+            email : st.session_state["email_d"], 
+            number : st.session_state["number_d"], 
+            birth_date : st.session_state["birth_date_d"], 
+            comments : st.session_state["Comm_d"]
+            }).execute()
