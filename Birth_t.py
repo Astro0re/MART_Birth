96c0cbf8bd683539455554b5552f754ca26d9d34
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2026-05-18 08:37:22 +0100

    Code refactor

--- Birth_t.py
+++ Birth_t.py
@@ -1,6 +1,6 @@
 # Packages 
 import streamlit as st
-
+import func.py 
 
 def main():
     st.title("MART BABIES")
@@ -13,32 +13,10 @@ def main():
     birth_date = st.date_input("Date of Birth: ", key="birth_date_d")
     comments = st.text_area("Patient Comment", max_chars=150, placeholder="Comments", key='Comm_d')
 
-
-
-    def add_values(table_name : str ,data : dict ):
-
-        try: 
-            response = supabase.table(table_name).insert(data).execute()
-            if response.data:
-                st.text('Data added successfully')
-            else:
-                st.text('Error in Input')
-        except Exception as e:
-            print(f'Erro due to :{e}')  
+ 
 
     if st.button("Submit", key='SUB'):
-        for_data = {
-            "f_name" : st.session_state["f_name_d"],
-            "l_name" : st.session_state["l_name_d"], 
-            "cycle_id" : st.session_state["cycle_id_d"], 
-            "email" : st.session_state["email_d"], 
-            "number" : st.session_state["number_d"], 
-            "birth_date" : st.session_state["birth_date_d"], 
-            "comments" : st.session_state["Comm_d"]
-            }
-
-        return(for_data)
-        add_values('birth_track', data)
+        func.py.main()  
 
 
 if __name__ == "__main__":
